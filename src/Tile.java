9e3172d9b46304bf1d8c09986e18d001afdc6385
vinceguemat <33133940+vinceguemat@users.noreply.github.com>
2019-02-14 19:22:11 +0530

    Tile.init
    creation of the data structure and the constructor of class Point and class Tile

--- src/Tile.java
+++ src/Tile.java
@@ -1,4 +1,50 @@
+public enum Type
+{
+	EMPTY, SQUARE, CIRCLE, TIE, BOARD;
+}
 
-public class Tile {
-
+public class Tile
+{
+	Tile[][] board;
+	Type type;
+	int level;
+	Point selected;
+	
+	Tile(int level)
+	{
+		this.level = level;
+		this.selected = null;
+		if(level == 0)
+		{
+			this.type = Type.EMPTY;
+			this.board = null;
+		}
+		else
+		{
+			this.type = Type.BOARD;
+			this.board = new Tile[3][3];
+			for(int i = 0; i < 3; i++)
+			{
+				for(int j = 0; j <3; j++)
+				{
+					this.board[i][j] = new Tile(this.level - 1)
+				}
+			}
+		}
+	}
+	
+	Tile()
+	{
+		this.level = 2;
+		this.selected = null;
+		this.type = Type.BOARD;
+		this.board = new Tile[3][3];
+		for(int i = 0; i < 3; i++)
+		{
+			for(int j = 0; j <3; j++)
+			{
+				this.board[i][j] = new Tile(this.level - 1)
+			}
+		}
+	}
 }
