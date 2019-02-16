34b26e71785583a229e24385cbe54f0edd434210
vinceguemat <33133940+vinceguemat@users.noreply.github.com>
2019-02-16 12:08:07 +0530

    check
    the last turn is handled

--- src/Engine.java
+++ src/Engine.java
@@ -4,10 +4,13 @@ public class Engine {
 	private Player p;
 	private String save;
 	private boolean win;
+	private Point[] last;
 
 	public Engine() {
 		this.board = new Tile();
-
+		this.last = new Point[2];
+		for(int i = 0; i < 2; i++)
+			last[i] = null;
 		this.actual = this.board;
 		this.save = "";
 		this.p = Player.CROSS;
@@ -56,6 +59,7 @@ public class Engine {
 			if (Point.isValid(selected) && actual.getCell(selected).is_free()) {
 				actual = actual.getCell(selected);
 				if (actual.insert(p) == 1) {
+					updateLast();
 					temp.incrementNum();
 					temp.select(null);
 					if (temp.winOrTie(p, selected))
@@ -71,6 +75,22 @@ public class Engine {
 					}
 				}
 			}
+		}
+	}
+
+	public void updateLast()
+	{
+		if (last[0] == null)
+		{
+			last[0] = new Point(board.getSelected());
+		} else {
+			last[0].copy(board.getSelected());
+		}
+		if (last[1] == null)
+		{
+			last[1] = new Point(board.getCell(board.getSelected()).getSelected());
+		} else {
+			last[1].copy(board.getCell(board.getSelected()).getSelected());
 		}
 	}
 }
